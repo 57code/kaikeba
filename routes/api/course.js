9980ef9479f1e6df0f634dfb339bdc6f9610e8f5
yt0379 <yt0379@qq.com>
2018-12-05 23:29:41 +0800

    注销、搜索课程

--- routes/api/course.js
+++ routes/api/course.js
@@ -4,8 +4,9 @@ const {query} = require('../../models/db');
 
 router.get('/search', async (req, res) => {
     try {
-        const sql = `SELECT * FROM kkb.vip_course where name like '%${req.query.keyword}%'`;
-        const results = await query(sql);
+        const sql = `SELECT * FROM kkb.vip_course where name like ?`;
+        // 请这样传条件
+        const results = await query(sql, '%'+req.query.keyword+'%');
         res.json({success: true, data: results})
     } catch (error) {
         res.json({success: false, message: '搜索错误，请重试！'})
